505af5d00c58cc20b71eb0bc2fecb01c7036702a
Ruben Eduardo Tejeda Roa <rubentejedaroa@gmail.com>
2019-05-04 10:31:49 -0400

    init project

---
+++ Assets/JsonCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonCtrl : MonoBehaviour {
+    public JSONObject jsonObject;
+    public string texto;
+	// Use this for initialization
+	void Start () {
+        jsonObject = new JSONObject(texto);
+        Debug.Log(jsonObject["fac"]["tie"]["asde"]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
